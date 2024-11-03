2ec1198ab0848e04654c3989a1f318475fa823c2
bayzedahmedandthe <bayzedm784@.com>
2024-11-03 20:40:12 +0600

    start assaignment-08

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Bayzed</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './Components/Header/Header';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>,
+  },
+]);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
